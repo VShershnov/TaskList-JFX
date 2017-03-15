479457c888836fb6a66dd6eaa16e721e9ad4c88a
VShershnov <vshershnyov@gmail.com>
2017-03-15 10:25:43 +0200

    change Task constructor by write memory

--- src/tasklist/model/Task.java
+++ src/tasklist/model/Task.java
@@ -52,7 +52,7 @@ public class Task {
     public Task(String name, String memory) {
         this.name = new SimpleStringProperty(name);
         
-        this.pID = new SimpleStringProperty("");
+        this.pID = new SimpleStringProperty(memory);
         //this.pID = new SimpleStringProperty(memory);
         // Какие-то фиктивные начальные данные для удобства тестирования.
         
